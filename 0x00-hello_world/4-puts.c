2ca75aa8e3aaf3ac3c7e3b77599569db9dd90cad
keju03 <raimotakeju2003@gmail.com>
2022-09-13 15:13:34 -0700

    writes c codes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-/**
- * main - Entry point
- *
- * Description: main - Prints out a string to stdout.
- *
- * Return: 0 if success.
- */
 
+/**
+ *main - Entry point
+ *
+ * Description: prints "Programming is like building a multilingual puzzle"
+ *
+ *Return: Always 0 (success)
+ */
 int main(void)
 {
-         puts("\"Programming is like building a multilingual puzzle");
-         return (0);
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
