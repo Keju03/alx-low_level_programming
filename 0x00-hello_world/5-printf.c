8cdbf30f5a24103d0bb0d2f63fbb09491be86747
keju03 <raimotakeju2003@gmail.com>
2022-09-14 08:34:52 -0700

    writes c codes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,12 @@
 /**
  *main - Entry point
  *
- *Description:prints "with proper grammer, but the outcome is a piece of art"
+ *Description:prints "with proper grammer, but the outcome is a piece of art,"
  *
  *Return: Always 0 (success)
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
