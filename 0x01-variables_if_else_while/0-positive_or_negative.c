58895734eceaf073bfde39a5c9b154b12bbaf5bf
keju03 <raimotakeju2003@gmail.com>
2022-09-14 15:02:02 -0700

    writes variables,if,else,while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,2 +1,32 @@
-#!/bin/bash
-gcc -E $CFILE -o c
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print whether the number is positive or negative
+ *
+ * Return: Always 0 (Succes)
+ */
+
+int main(void)
+{
+
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
