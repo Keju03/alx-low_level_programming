6ad014a03f3eaa2abf820cbcbed21ee85de01ff5
keju03 <raimotakeju2003@gmail.com>
2022-09-15 15:20:20 -0700

    c functions,nested loopd

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - This program prints the phrase Hello, World
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+
+	char ch[] = "_putchar";
+	int i;
+
+	for (i = 0; ch[i] != '\0'; 1++)
+	{
+		_putchar(ch[i]);
+	}
+
+	_putchar('\n');
+	return (0);
+}
