355b760670d349502e46487e1685c881149280e3
keju03 <raimotakeju2003@gmail.com>
2022-09-15 05:26:38 -0700

    writes c variables,if,else,while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print single digits of base
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		putchar(c);
+	}
+	putchar(10);
+	return(0);
+}
