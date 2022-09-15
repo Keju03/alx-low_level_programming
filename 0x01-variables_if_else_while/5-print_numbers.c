457896e3987f8afa2f136a4dd2d4f66e092cadc8
keju03 <raimotakeju2003@gmail.com>
2022-09-15 05:16:13 -0700

    writes c variables,if,else,while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#inlude <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print all single digits
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
