10ca67dc77415af98c6e2bf4d51a53c344c7a54c
keju03 <raimotakeju2003@gmail.com>
2022-09-15 05:03:46 -0700

    writes c variables,if,while,else

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints alphabets in lowercase except e and p
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 'a';
+	while (i <= 'z')
+	{
+		if (i != 'e' && i != 'q')
+		{
+			putchar(i);
+		}
+		i++;
+	}
+	putchar{'\n');
+	return (0);
+}
